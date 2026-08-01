cda61035c861d238da2636f48c1c7f16a9e5c99f
madandevops11 <126912468+madandevops11@users.noreply.github.com>
2026-08-01 21:08:55 +0530

    Create variables.tf

---
+++ terraform/ec2_multi/variables.tf
@@ -0,0 +1,11 @@
+ariable "configuration" {
+  description = "EC2 configuration"
+
+  type = list(object({
+    application_name       = string
+    instance_type          = string
+    no_of_instances        = number
+    subnet_id              = string
+    vpc_security_group_ids = list(string)
+  }))
+}
